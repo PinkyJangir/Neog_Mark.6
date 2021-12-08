64d06cac76754160dc55b39ea8d1b96392c7e42d
PinkyJangir <pinkyjangir634@gmail.com>
2021-12-08 19:46:14 +0530

    intial

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    console.log(serverURL + "?" + "text=" + input)
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured",error);
+    alert("somthing wrong with server! try again after some time")
+}
+
+function clickHandler() {
+    // var inputText=txtInput.value; //taking input    
+
+    // calling server for processing
+    fetch(getTranslationURL(txtInput.value))
+        .then(response => response.json())
+        .then(json=> console.log(json))
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
